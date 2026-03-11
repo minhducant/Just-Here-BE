ebb440cef426a6a772648bd2ccbe6624f8610be7
Đỗ Minh Đức <minhduc.gco@gmail.com>
2026-03-12 00:26:39 +0700

    refactor: remove payment module and related services, controllers, and schemas

--- src/modules.ts
+++ src/modules.ts
@@ -11,14 +11,9 @@ import { mongodb } from 'src/configs/database.config';
 import { redisConfig } from 'src/configs/redis.config';
 import { JUST_HERE_QUEUE } from 'src/shares/queue/justhere.queue';
 //Customer Module
-import { NoteModule } from 'src/modules/note/note.module';
 import { UserModule } from 'src/modules/user/user.module';
 import { AuthModule } from 'src/modules/auth/auth.module';
-import { DebtModule } from 'src/modules/debt/debt.module';
-import { ToolModule } from 'src/modules/tool/tool.module';
-import { InviteModule } from 'src/modules/invite/invite.module';
 import { FriendModule } from 'src/modules/friend/friend.module';
-import { PaymentModule } from 'src/modules/payment/payment.module';
 import { MessagesModule } from 'src/modules/messages/messages.module';
 import { JustHereModule } from 'src/modules/justhere/justhere.module';
 import { NotificationModule } from 'src/modules/notification/notification.module';
@@ -44,13 +39,8 @@ const Modules: any = [
   AuthModule,
   UserModule,
   FriendModule,
-  InviteModule,
-  NoteModule,
   MessagesModule,
-  DebtModule,
-  PaymentModule,
   NotificationModule,
   JustHereModule,
-  ToolModule,
 ];
 export default Modules;
